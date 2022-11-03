5fbc70d265d6613b21a562c552945d5e2f6a45a8
Ironfistb <brian_the_first89@hotmail.com>
2022-11-03 17:39:32 -0300

    Actualizacion ticket compra v.1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
-var a = 1
+
+
+const campoNumerico = document.getElementById('cantidad');
+
+campoNumerico.addEventListener('keydown', function(evento) {
+  const teclaPresionada = evento.key;
+  const teclaPresionadaEsUnNumero =
+    Number.isInteger(parseInt(teclaPresionada));
+
+  const sePresionoUnaTeclaNoAdmitida = 
+    teclaPresionada != 'ArrowDown' &&
+    teclaPresionada != 'ArrowUp' &&
+    teclaPresionada != 'ArrowLeft' &&
+    teclaPresionada != 'ArrowRight' &&
+    teclaPresionada != 'Backspace' &&
+    teclaPresionada != 'Delete' &&
+    teclaPresionada != 'Enter' &&
+    !teclaPresionadaEsUnNumero;
+  const comienzaPorCero = 
+    campoNumerico.value.length === 0 &&
+    teclaPresionada == 0;
+
+  if (sePresionoUnaTeclaNoAdmitida || comienzaPorCero) {
+    evento.preventDefault(); 
+  }
+});
+
+function mostrarTotal(){  
+    let cat = document.getElementById("categoria").value;
+    //console.log(cat);
+    let precio = 200;
+    let cantidad = parseInt(document.getElementById('cantidad').value);    
+    let total1 = precio * cantidad;
+    console.log(total1);
+    let total = total1 - (total1 * cat)
+    console.log(total);
+    document.getElementById('total').value = `Total a pagar: \$${total}`;
+}
+
+function limpiarTodo(){    
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('cantidad').value = '1';
+}
+
+/*function cat() {
+    var cate = document.getElementById("categoria").value;
+    console.log(cate)
+}   */
+
+
+
+
+
+document.getElementById('btn_calcular').addEventListener('click', mostrarTotal);
+document.getElementById('borrar').addEventListener('click', limpiarTodo);
+// let cat = document.getElementById("categoria").value;
